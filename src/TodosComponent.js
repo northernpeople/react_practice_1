06c5b9e0499e96997fe611a83d7fd7eaf9b25566
Stepan Razdobarov <northernpeople@gmail.com>
2017-12-04 09:07:03 -0600

    added and imported first component

---
+++ src/TodosComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+
+class TodosComponent extends Component {
+    
+    render(){
+        return(
+            <ul>
+                <li>hi component!</li>
+            </ul>
+        )
+    }
+}
+
+export default TodosComponent;
